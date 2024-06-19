7ae09bed2ced791414849e50ace88e4ff66381f3
bspt628 <hirotobspt628@gmail.com>
2024-06-19 18:25:19 +0900

    log tuketa

--- client_disconnect_2complete.c
+++ client_disconnect_2complete.c
@@ -10,8 +10,12 @@
 #include <pthread.h>
 #include <string.h>
 
-#define CSERVER_PORT 70000
+#define CSERVER_PORT 50000
 #define BUFFER_SIZE 8192
+
+// IP.txt IPアドレスを取得
+// log.txt ログはなんでもここに
+// stopper.txt ここに1が入ったら終了
 
 void check_stopper(int ichi);
 void write_stopper();
@@ -19,25 +23,25 @@ void write_to_log(char* FILE_NAME, char* str, int num);
 
 void check_stopper(int ichi) {
     // if stopper.txt has 1, then stop the program
-    FILE *f = fopen("stopper.txt", "r");
+    FILE *stopfile_read = fopen("stopper.txt", "r");
     char c;
-    if (f == NULL) {
+    if (stopfile_read == NULL) {
         return;
     }
-    c = fgetc(f);
+    c = fgetc(stopfile_read);
     if (c == '1') {
         write_to_log("log.txt", "Stopper ni yotte process ga shuuryou saremashita!", getpid());
         exit(0);
     }
-    fclose(f);
+    fclose(stopfile_read);
     return;
 }
 
 void write_stopper() {
     // Write to file "stopper.txt"
-    FILE *f = fopen("stopper.txt", "w");
-    fprintf(f, "1");
-    fclose(f);
+    FILE *stopfile_write = fopen("stopper.txt", "w");
+    fprintf(stopfile_write, "1");
+    fclose(stopfile_write);
     return;
 }
 
@@ -119,107 +123,124 @@ int main(int argc, char** argv){
         exit(1);
     }
     remove("log.txt");
+    FILE * f_log = fopen("log.txt","w");
     remove("stopper.txt");
 
     FILE * IP = fopen("IP.txt","w");
 
-    int s = socket(PF_INET,SOCK_STREAM,0);
+    // central serverと接続するためのソケットを作る
+    int socket_to_CS = socket(PF_INET,SOCK_STREAM,0);
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = inet_addr(argv[1]);
     addr.sin_port = htons(atoi(argv[2]));
 
-    int ret = connect(s,(struct sockaddr *)&addr, sizeof(addr));
+    // CS(central server)とconnect
+    int ret = connect(socket_to_CS,(struct sockaddr *)&addr, sizeof(addr));
     if (ret == -1){
         perror("connect");
         exit(1);
     }
 
+    // すでにつながっているパソコンのIPアドレスをCSから読み込む
     while(1){
         char string[1];
-        int n = recv(s,string,1,0);
+        int n = recv(socket_to_CS,string,1,0);
         if(n == 0){
             break;
         }
         fwrite(string,1,1,IP);
     }
-
     fclose(IP);
     IP = fopen("IP.txt","r");
     if(IP == NULL){
         perror("fopen");
         exit(1);
     }
+    // CSとの接続を切断する
+    close(socket_to_CS);
 
-    // disconnect from central server
-    close(s);
+    // check_stopper(4);
 
-    check_stopper(4);
-
+    // CSからもらったIPアドレスを順に読み込む
     char* IP_addr = malloc(sizeof(char)*20);
-    FILE * f_log = fopen("log.txt","w");
+    
+
+    // すでにつながっているパソコンの数だけ
     while(fgets(IP_addr, 20, IP) != NULL) {
         check_stopper(5);
-        fprintf(f_log,"%s\n",IP_addr);
-        fprintf(f_log, "while ni kita yo!, Process NO. %d\n", getpid());
 
-        write_to_log("log.txt", "ima kara fork suru yo!", getpid());
+        fprintf(f_log,"[initial client] to shite %s wo yomi kondayo ! [Process No. %d]\n",IP_addr, getpid());
+        fclose(f_log);
+        
+        write_to_log("log.txt", "[initial client] ima kara fork suru yo!", getpid());
+        /* forkの分担 
+        親プロセス: 次のIPアドレスを読み込んで 読み込み終わったら serverとしての役割にシフトする
+        子プロセス: 読み込んだIPアドレスを使って通話をする
+        */ 
         int qid = fork();
-        write_to_log("log.txt", "fork dekita yo!", getpid());
+        write_to_log("log.txt", "[initial client] fork dekita yo!", getpid());
         // write to log that
         if(qid == 0){
+            // 以下子プロセスの挙動
+            // IPアドレスをトリムする
             lntrim(IP_addr);
-            int s = socket(PF_INET,SOCK_STREAM,0);
+            // phone serverと接続するためのソケットを作る
+            int s_to_PS = socket(PF_INET,SOCK_STREAM,0);
             struct sockaddr_in addr;
 
             addr.sin_family = AF_INET;
             addr.sin_addr.s_addr = inet_addr(IP_addr);
             addr.sin_port = htons(CSERVER_PORT);
-            int ret = connect(s,(struct sockaddr *)&addr, sizeof(addr));
+            // phone serverとconnect
+            int ret = connect(s_to_PS,(struct sockaddr *)&addr, sizeof(addr));
             if (ret == -1){
                 perror("connect");
             exit(1);
             }
-            call(s);
+            write_to_log("log.txt", "[initial client] PS to call hazimeruyo!", getpid());
+            // phone serverと通話する
+            call(s_to_PS);
         }
     }
-    write_to_log("log.txt", "while nuketa yo!", getpid());
 
+    // ここからはphone serverとしての役割にシフトする
+    write_to_log("log.txt", "[phone server] PS ni narimashita!", getpid());
 
-    FILE *f = fopen("./log_queue.txt", "a");
-    fprintf(f, "dekita \n");
-    fclose(f);
-    int ss = socket(PF_INET, SOCK_STREAM,0);
-    int a = CSERVER_PORT;
+    // この後入ってきたクライアント(phone client: PC)と通話するソケットを作る
+    int socket_to_PC = socket(PF_INET, SOCK_STREAM,0);
 
-    write_to_log("log.txt", "kore kara while hairu yo!", getpid());
     while(1){
-        write_to_log("log.txt", "while ni kita yo!", getpid());
+        write_to_log("log.txt", "[phone server] client matteruyo!", getpid());
         check_stopper(100);
         struct sockaddr_in addr;
         addr.sin_family = AF_INET;
-        addr.sin_port = htons(a);
+        addr.sin_port = htons(CSERVER_PORT);
         addr.sin_addr.s_addr = INADDR_ANY;
-        bind(ss, (struct sockaddr *)&addr, sizeof(addr));
 
-        listen(ss,10);
+        // socket_to_PCを有効化する（ポート番号を変える必要があるかも？）
+        bind(socket_to_PC, (struct sockaddr *)&addr, sizeof(addr));
+
+        // PCを待ち受ける
+        listen(socket_to_PC,10);
 
         struct sockaddr_in client_addr;
         socklen_t len = sizeof(struct sockaddr_in);
 
-        int s = accept(ss, (struct sockaddr *)&client_addr, &len);
-        write_to_log("log.txt", "ima kara server toshite fork suru yo!", getpid());
+        // 
+        int s = accept(socket_to_PC, (struct sockaddr *)&client_addr, &len);
+        write_to_log("log.txt", "[phone server] client kiya!! \nima kara PS toshite fork suru yo!", getpid());
         int pid = fork();
         check_stopper(101);
         if(pid == 0){
-            write_to_log("log.txt", "Server toshite clal suru yo", getpid());
+            write_to_log("log.txt", "[phone server] Server toshite call suru yo", getpid());
             call(s);
             check_stopper(6);
         }
         check_stopper(102);
         close(s);
     }
-    write_to_log("log.txt", "saigo no while nuketa yo!", getpid());
+    write_to_log("log.txt", "[phone server] PS no while nuketa yo!", getpid());
     free(IP_addr);
     check_stopper(7);
 
